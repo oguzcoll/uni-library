775fdb7c6619245fa002784ce3e7d185cf5441b1
Oğuz Çolak <114251468+oguzcoll@users.noreply.github.com>
2025-01-15 23:44:08 +0300

    book card list implemented

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import BookCard from './BookCard';
+interface Props {
+  title: string;
+  books: Book[];
+  containerClassName?: string;
+}
 
-const BookList = ({ title }: any) => {
+const BookList = ({ title, books, containerClassName }: Props) => {
   console.log(title);
   return (
-    <section>
-      <h2 className='font-bebas-neue text-4xl text-light-400'>Popular books</h2>
+    <section className={containerClassName}>
+      <h2 className='font-bebas-neue text-4xl text-light-400'>{title}</h2>
+      <ul className='book-list'>
+        {books.map((book) => (
+          <BookCard key={book.title} {...book} />
+        ))}
+      </ul>
     </section>
   );
 };
