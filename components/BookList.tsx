94dcd0cc6dd507009bbfc6670a8cb45431448942
Oğuz Çolak <114251468+oguzcoll@users.noreply.github.com>
2025-01-15 20:51:21 +0300

    updated

--- components/BookList.tsx
+++ components/BookList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const BookList = () => {
+const BookList = ({ title }: any) => {
+  console.log(title);
   return (
     <section>
       <h2 className='font-bebas-neue text-4xl text-light-400'>Popular books</h2>
