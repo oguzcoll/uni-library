aa3661736bc3c54a838dda2e1db4b9ac79126884
Oğuz Çolak <114251468+oguzcoll@users.noreply.github.com>
2025-01-19 22:12:32 +0300

    seed dummy data to db

--- app/(route)/my-profile/page.tsx
+++ app/(route)/my-profile/page.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 import { signOut } from '@/auth';
 import BookList from '@/components/BookList';
-import { sampleBooks } from '@/constants';
 
 const Page = () => {
   return (
@@ -18,7 +17,7 @@ const Page = () => {
         <Button>Logout</Button>
       </form>
 
-      <BookList title='Borrowed Books' books={sampleBooks} />
+      {/* <BookList title='Borrowed Books' books={sampleBooks} /> */}
     </>
   );
 };
